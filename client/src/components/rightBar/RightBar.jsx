8c75bc54175a043048c41ffed0be5945b044a175
ashish nagar <ashishnagar033@gmail.com>
2023-04-04 23:43:28 +0530

    sidebar item updated

--- client/src/components/rightBar/RightBar.jsx
+++ client/src/components/rightBar/RightBar.jsx
@@ -47,6 +47,19 @@ const RightBar = () => {
               <button>dismiss</button>
             </div>
           </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                alt="profile"
+                src="https://images.pexels.com/photos/15597897/pexels-photo-15597897.jpeg?cs=srgb&dl=pexels-b%E1%BA%A3o-vi%E1%BB%87t-15597897.jpg&fm=jpg&w=640&h=960&_gl=1*qa7fxa*_ga*MTk5NDIxNjk4Ni4xNjc1NjU4Mzkw*_ga_8JE65Q40S6*MTY4MDQ1MDk3Mi40LjEuMTY4MDQ1MDk4My4wLjAuMA.."
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
