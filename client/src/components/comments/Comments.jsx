0ef06d6564ba7e9def8b824fc9e988042b4a9d72
ashish nagar <ashishnagar033@gmail.com>
2023-04-09 23:51:53 +0530

    input box added for comments

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./comments.scss";
 
 const Comments = () => {
@@ -31,8 +31,16 @@ const Comments = () => {
     },
   ];
 
+  const currentUser =
+    "https://images.pexels.com/photos/15597897/pexels-photo-15597897.jpeg?cs=srgb&dl=pexels-b%E1%BA%A3o-vi%E1%BB%87t-15597897.jpg&fm=jpg&w=640&h=960&_gl=1*qa7fxa*_ga*MTk5NDIxNjk4Ni4xNjc1NjU4Mzkw*_ga_8JE65Q40S6*MTY4MDQ1MDk3Mi40LjEuMTY4MDQ1MDk4My4wLjAuMA..";
+
   return (
     <div className="comments">
+      <div className="write-comment">
+        <img src={currentUser} alt="" />
+        <input type="text" placeholder="write a comment" />
+        <button>Send</button>
+      </div>
       {comments.map((comment) => (
         <div className="comment">
           <img alt="" src={comment.profilePic} />
