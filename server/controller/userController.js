9f9c0072d8580019fbfb505c6ac5dc53bc7fad96
ashish nagar <ashishnagar033@gmail.com>
2023-04-23 23:08:42 +0530

    backend : delete and follow user added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -40,7 +40,48 @@ const updateUser = async (req, res) => {
   }
 };
 
+///======================
+///   delete user
+///=====================
+const deleteUser = async (req, res) => {
+  try {
+    if (req.user.id === req.params.id) {
+      const user = await User.findByIdAndDelete(req.params.id, req.body);
+
+      res.status(200).json("user deleted ");
+    }
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
+///======================
+///   follow user
+///=====================
+const follow = async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.user.id);
+    const followerUser = await User.findById(req.params.id);
+
+    if (!currentUser.followers.includes(req.user.id)) {
+      await currentUser.updateOne({
+        $push: { followers: followerUser.id },
+      });
+      await followerUser.updateOne({
+        $push: { followings: currentUser.id },
+      });
+    } else {
+      res.status(403).json("You already follow this user");
+    }
+    res.status(200).json("Following the use");
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getUser,
   updateUser,
+  deleteUser,
+  follow,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { getUser, updateUser } = require("../controller/userController");
+const {
+  getUser,
+  updateUser,
+  deleteUser,
+  follow,
+} = require("../controller/userController");
 const verifyToken = require("../middlewares/verifyToken");
 
 const router = express.Router();
@@ -9,5 +14,11 @@ router.patch("/:id", verifyToken, updateUser);
 
 // get user
 router.route("/find/:id").get(getUser);
+
+// delete user
+router.delete("/find/:id", verifyToken, deleteUser);
+
+// follow user
+router.put("/follow/:id", verifyToken, follow);
 
 module.exports = router;
