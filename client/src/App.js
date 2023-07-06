0e496b09593c1304d8ef3b484654bfade8919161
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-06 15:30:12 +0530

    code cleaned

--- client/src/App.js
+++ client/src/App.js
@@ -1,91 +1,22 @@
-import LeftBar from "./components/leftBar/LeftBar";
-import Navbar from "./components/navbar/Navbar";
-import RightBar from "./components/rightBar/RightBar";
+
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Bookmarked from "./components/Bookmarked posts/Bookmarked"
 import {
   createBrowserRouter,
   createRoutesFromElements,
-
-  Outlet,
   Route,
   RouterProvider,
-  useNavigate,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import "./style.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { loginUser, selectUser } from "./features/userSlice";
 import UserConnections from "./pages/userConnections/UserConnections";
-import { useEffect } from "react";
-
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout/Layout";
 
 
 function App() {
-  const currentUser = useSelector(selectUser);
-  
-  const ProtectedRoute = ({ children }) => {
-    const navigate = useNavigate();
-  const dispatch = useDispatch()
-
-  
-        useEffect(() => {
-         
-          const checkUser = async () => {
-            try {
-              const data = await fetch("/api/v1/auth/getuser", {
-                method: "GET",
-                headers: {
-                  Accept: "application/json",
-                  "Content-Type": "application/json",
-                },
-                credentials: "include",
-              });
-              const jsonData = await data.json();
-    
-              if (data.ok) { 
-             
-                dispatch(loginUser(jsonData));
-               return navigate("/");
-              } else {
-                return navigate('/login')
-              }
-            } catch (error) {
-        console.log(error)
-            }
-          }
-          if (!currentUser) {
-            checkUser()
-        }
-        }, [])
-
-    return children;
-  };
-
-  const Layout = () => {
-    return (
-      <>
-        <Navbar />
-        <div
-          style={{
-            display: "flex",
-            padding: "1rem",
-            backgroundColor: " #f5f7f8",
-          }}
-        >
-          <LeftBar />
-          <div className="centerBar">
-            <Outlet />
-          </div>
-          <RightBar />
-        </div>
-      </>
-    );
-  };
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
