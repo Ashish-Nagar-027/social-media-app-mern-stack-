71d3f354da8491b7a14285aaebc87a599a0bf887
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-06 15:33:57 +0530

    layout component added

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './layout.scss'
+import { Outlet } from 'react-router-dom';
+import RightBar from '../rightBar/RightBar';
+import LeftBar from '../leftBar/LeftBar';
+import Navbar from '../navbar/Navbar';
+
+const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <div
+          style={{
+            display: "flex",
+            padding: "1rem",
+            backgroundColor: " #f5f7f8",
+          }}
+        >
+          <LeftBar />
+          <div className="centerBar">
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </>
+    );
+  };
+
+export default Layout
