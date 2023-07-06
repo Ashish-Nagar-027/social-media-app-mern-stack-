012d4c47b05fe167a0487541d211ed7a0ad96dcd
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-06 15:31:22 +0530

    protectect route componenet added

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { loginUser, selectUser } from "../features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const currentUser = useSelector(selectUser)?._id
+
+
+  useEffect(() => {
+    const checkUser = async () => {
+        try {
+          const data = await fetch("/api/v1/auth/getuser", {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          });
+          const jsonData = await data.json();
+
+          if (data.ok) {
+            dispatch(loginUser(jsonData));
+            return navigate("/");
+          } else {
+            return navigate("/login");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    if (!currentUser) {
+      checkUser();
+    }
+  }, [currentUser,dispatch, navigate]);
+
+
+  return children;
+};
+
+export default ProtectedRoute;
