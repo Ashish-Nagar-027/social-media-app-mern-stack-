355bf90521deb8516567e46d64c7bec05eb3675f
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-26 19:06:59 +0530

    minor changes and bug  fixes

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -7,37 +7,35 @@ const ProtectedRoute = ({ children }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const currentUser = useSelector(selectUser)?._id
-
+  const currentUser = useSelector(selectUser)?._id;
 
   useEffect(() => {
     const checkUser = async () => {
-        try {
-          const data = await fetch("/api/v1/auth/getuser", {
-            method: "GET",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            credentials: "include",
-          });
-          const jsonData = await data.json();
+      try {
+        const data = await fetch("/api/v1/auth/getuser", {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        const jsonData = await data.json();
 
-          if (data.ok) {
-            dispatch(loginUser(jsonData));
-            return navigate("/");
-          } else {
-            return navigate("/login");
-          }
-        } catch (error) {
-          console.log(error);
+        if (data.ok) {
+          dispatch(loginUser(jsonData));
+          return navigate("/");
+        } else {
+          return navigate("/login");
         }
-      };
+      } catch (error) {
+        console.log(error);
+      }
+    };
     if (!currentUser) {
       checkUser();
     }
-  }, [currentUser,dispatch, navigate]);
-
+  }, [currentUser, dispatch, navigate]);
 
   return children;
 };
