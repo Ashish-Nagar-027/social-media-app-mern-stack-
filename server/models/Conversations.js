b21856a8f99d5568f2479a19c1d81152082421cf
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-28 23:48:50 +0530

    conversation model created

---
+++ server/models/Conversations.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+const conversationSchema = new mongoose.Schema({
+    members: {
+        type: Array
+    }
+}, {
+    timestamps: true
+}
+)
+
+
+module.exports = mongoose.Model("Conversation", conversationSchema)
