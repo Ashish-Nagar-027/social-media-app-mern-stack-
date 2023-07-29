f320830ce9b4bd485db096dedb8c3bd95b4f2baa
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-30 00:31:29 +0530

    fetching user info from server added

--- client/src/components/messages_profile/Message_profile.jsx
+++ client/src/components/messages_profile/Message_profile.jsx
@@ -1,29 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
 
 import "./message_profile.scss";
+import { selectUser } from "../../features/userSlice";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
-const Message_profile = () => {
-  const user = useState([]);
+const Message_profile = ({ user }) => {
+  // console.log(user);
+  const currentUser = useSelector(selectUser);
+  const otherUserId = user?.members.filter(
+    (userId) => userId !== currentUser._id
+  );
 
-  return (
-    <div className="msg-profile-user" key={user._id || "random"}>
-      <div className="userInfo">
+  const [msgUser, setMsgUser] = useState();
+
+  useEffect(() => {
+    const fetchDataFunction = async () => {
+      const fetchData = await axios.get(
+        `http://localhost:3001/api/v1/user/${otherUserId[0]}`
+      );
+      setMsgUser(fetchData.data);
+    };
+    fetchDataFunction();
+  }, []);
+
+  if (msgUser) {
+    return (
+      <div className="msg-profile-user">
         <div className="userInfo">
-          {user?.profilePic?.url ? (
-            <img
-              className="profile-img"
-              src={user.profilePic.url}
-              alt={user.name}
-            />
-          ) : (
-            <CgProfile size={30} />
-          )}
-          <span>{user.name || "Ashish nagar"} </span>
+          <div className="userInfo">
+            {msgUser.profilePic?.url ? (
+              <img
+                className="profile-img"
+                src={msgUser.profilePic.url}
+                alt={msgUser.name}
+              />
+            ) : (
+              <CgProfile size={30} />
+            )}
+            <span>{msgUser.name} </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Message_profile;
