30b54ad0d8634c9fc388337314ecf25e7617cb31
Ashish Nagar <Ashishnagar033@gmail.com>
2023-07-30 23:08:43 +0530

    conversation slice added

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import profileUserReducer from "../features/proFileUserSlice";
 import postsReducer from "../features/postSlice";
+import conversationReducer from "../features/conversationSlice"
 
 export default configureStore({
   reducer: {
     user: userReducer,
     profileUser: profileUserReducer,
     posts: postsReducer,
+    conversations: conversationReducer
   },
 });
