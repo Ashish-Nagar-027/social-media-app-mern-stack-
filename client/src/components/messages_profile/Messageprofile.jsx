7d6c688cf7cda931eb0b59725e30822d2a7d2104
Ashish Nagar <Ashishnagar033@gmail.com>
2023-08-26 19:39:44 +0530

    minor changes and code fixes

---
+++ client/src/components/messages_profile/Messageprofile.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import { CgProfile } from "react-icons/cg";
+
+import "./message_profile.scss";
+import { selectUser } from "../../features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import {
+  selectConversationUser,
+  setConversationUser,
+} from "../../features/conversationSlice";
+
+const MessageProfile = ({ user }) => {
+  // console.log(user);
+  const currentUser = useSelector(selectUser);
+  const otherUserId = user?.members.filter(
+    (userId) => userId !== currentUser._id
+  );
+
+  const dispatch = useDispatch();
+  const conversationsUser = useSelector(selectConversationUser);
+
+  useEffect(() => {
+    const fetchDataFunction = async () => {
+      const fetchData = await axios.get(
+        `http://localhost:3001/api/v1/user/${otherUserId[0]}`
+      );
+
+      dispatch(setConversationUser(fetchData.data));
+    };
+    fetchDataFunction();
+  }, [dispatch, otherUserId]);
+
+  if (conversationsUser) {
+    return (
+      <div className="msg-profile-user">
+        <div className="userInfo">
+          <div className="userInfo">
+            {conversationsUser.profilePic?.url ? (
+              <img
+                className="profile-img"
+                src={conversationsUser.profilePic.url}
+                alt={conversationsUser.name}
+              />
+            ) : (
+              <CgProfile size={30} />
+            )}
+            <span>{conversationsUser.name} </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default MessageProfile;
