b3a19c0a9b2466ae5dc80bf98091e93dd418d972
Ashish Nagar <Ashishnagar033@gmail.com>
2023-09-19 20:33:23 +0530

    custom hook feature  added

--- client/src/pages/userConnections/UserConnections.jsx
+++ client/src/pages/userConnections/UserConnections.jsx
@@ -1,60 +1,21 @@
-import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { MdArrowBack } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 import {
   selectProfileUser,
   selectProfileUserFollowers,
   selectProfileUserfollowings,
-  setProfileUser,
-  setUserFollowers,
-  setUserFollowings,
 } from "../../features/proFileUserSlice";
 import "./userConnections.scss";
 import HandleFollowBtn from "../../components/following Button/HandleFollowBtn";
 import LoadingData from "../../components/LoadingData";
+import useHandleConnections from "../../hooks/useHandleConnections";
 
 const UserConnections = () => {
-  const params = useParams();
   const navigate = useNavigate();
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const [fetching, setFetching] = useState(false);
 
-  const [showFollowers, setShowFollowers] = useState(
-    location.pathname.split("/").at(-1)
-  );
-
-  const fetchDataFunction = useCallback(async () => {
-    const fetchData = await axios.get(
-      `http://localhost:3000/api/v1/user/${params.id}`
-    );
-    dispatch(setProfileUser(fetchData.data));
-  }, [params.id]);
-
-  const getUserInfo = useCallback(async () => {
-    setFetching(true);
-    const url =
-      "http://localhost:3000/api/v1/user/" + params.id + "/get" + showFollowers;
-
-    const getUser = await axios.get(url);
-    console.log(getUser.data);
-
-    if (showFollowers === "followers") {
-      dispatch(setUserFollowers(getUser.data));
-    }
-    if (showFollowers !== "followers") {
-      dispatch(setUserFollowings(getUser.data));
-    }
-    setFetching(false);
-  }, [dispatch, params.id]);
-
-  useEffect(() => {
-    setShowFollowers(location.pathname.split("/").at(-1));
-    getUserInfo();
-    fetchDataFunction();
-  }, [location.pathname, getUserInfo, fetchDataFunction]);
+  const { fetching, showFollowers } = useHandleConnections();
 
   const profileUser = useSelector(selectProfileUser);
   const profileUserFollowings = useSelector(selectProfileUserfollowings);
@@ -63,9 +24,6 @@ const UserConnections = () => {
     showFollowers === "followers"
       ? profileUserFollowers
       : profileUserFollowings;
-
-  const defaultProfilePic =
-    "https://images.pexels.com/photos/15597897/pexels-photo-15597897.jpeg?cs=srgb&dl=pexels-b%E1%BA%A3o-vi%E1%BB%87t-15597897.jpg&fm=jpg&w=640&h=960&_gl=1*qa7fxa*_ga*MTk5NDIxNjk4Ni4xNjc1NjU4Mzkw*_ga_8JE65Q40S6*MTY4MDQ1MDk3Mi40LjEuMTY4MDQ1MDk4My4wLjAuMA..";
 
   return (
     <div className="connections">
@@ -114,7 +72,15 @@ const UserConnections = () => {
                 <div className="user" key={user._id}>
                   <div className="userInfo">
                     <Link className="userInfo" to={`/profile/${user._id}`}>
-                      <img alt="profile" src={defaultProfilePic} />
+                      {user.profilePic?.url ? (
+                        <img
+                          className="profile-img"
+                          src={user.profilePic.url}
+                          alt={user.name}
+                        />
+                      ) : (
+                        <CgProfile size={30} />
+                      )}
                       <span>{user.name}</span>
                     </Link>
                   </div>
