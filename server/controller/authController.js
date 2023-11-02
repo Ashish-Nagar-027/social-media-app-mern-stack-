45b7d7ec473f1b3ec2e600c5af559cadeb7c6209
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-02 10:06:25 +0530

    login as guest feature added

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -101,6 +101,46 @@ const logout = async (req, res) => {
     .json("User has been logOut");
 };
 
+
+///====================
+//   Login As Guest 
+///=====================
+const loginAsGuest = async (req, res) => {
+  try {
+   
+    //checking existing
+    // name : Guest User ||  mail : GuestUser@mail.com ||  
+    const password =  'GuestUsersPassword'
+
+    const user = await User.findOne({email : 'GuestUser@mail.com'});
+    if (!user) {
+      throw Error("email not available , try sign up");
+    }
+
+    // compare password
+    const isRealPassword = await bcrypt.compare(password, user.password);
+    if (!isRealPassword) {
+      throw Error("wrong password");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+
+    user.password = undefined;
+
+    res
+  .cookie("accessToken", token, {
+    httpOnly: true,
+    secure: true,    
+    sameSite: 'None', 
+  })
+  .status(200)
+  .json(user);
+      
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 ///====================
 //         get current User
 ///=====================
@@ -133,5 +173,5 @@ module.exports = {
   getCurrentUser,
   register,
   login,
-  logout,
+  logout,loginAsGuest
 };
