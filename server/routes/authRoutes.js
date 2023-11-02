65055fd43de9283014151a4a36c35f24c451104c
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-02 10:06:55 +0530

    guest login route added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register, login, logout, getCurrentUser } = require("../controller/authController");
+const { register, login, logout, getCurrentUser, loginAsGuest,  } = require("../controller/authController");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/getUser", getCurrentUser);
+router.get("/guestlogin", loginAsGuest);
 
 module.exports = router;
