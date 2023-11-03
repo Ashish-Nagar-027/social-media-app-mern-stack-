fe6adc02185dabb9b186cae8c42aadb76802a231
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-03 21:48:12 +0530

    edit user modal component moved on top level

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,14 @@
 import React, { memo } from "react";
 import "./home.scss";
 import Posts from "../../components/Posts/Posts";
-import { editCurrentUser, selectUser } from "../../features/userSlice";
 import { useSelector } from "react-redux";
-import EditUserInfo from "../../components/Edit-current -user/EditUserInfo";
+import { selectUser } from "../../features/userSlice";
 
 const Home = () => {
   const currentUser = useSelector(selectUser);
-  const editUserInfo = useSelector(editCurrentUser);
 
   return (
     <div className="home">
-      {editUserInfo && <EditUserInfo />}
       <Posts id={currentUser?._id} />
     </div>
   );
