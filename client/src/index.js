d1b576114fd26211e1b54aaad096a66a7c5a3cd8
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-03 21:45:12 +0530

    skeleton theme color added

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <SkeletonTheme baseColor="#DDDBDD" highlightColor="rgba(130, 130, 130, 0.5)">
+        <App />
+      </SkeletonTheme>
     </Provider>
    </React.StrictMode>
 );
