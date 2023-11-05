c7c15a22c5e847c23c1cc282c8064231464ab628
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-05 18:36:33 +0530

    showsidebar reducer added

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,12 +3,14 @@ import userReducer from "../features/userSlice";
 import profileUserReducer from "../features/proFileUserSlice";
 import postsReducer from "../features/postSlice";
 import conversationReducer from "../features/conversationSlice"
+import showSideBarReducer from "../features/showSideBarSlice"
 
 export default configureStore({
   reducer: {
     user: userReducer,
     profileUser: profileUserReducer,
     posts: postsReducer,
-    conversations: conversationReducer
+    conversations: conversationReducer,
+    showSideBar: showSideBarReducer
   },
 });
