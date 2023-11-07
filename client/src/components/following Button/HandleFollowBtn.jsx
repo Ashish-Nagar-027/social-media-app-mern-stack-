5f591f8d4afa7768b3b43397749443ccfa4a3ac0
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-07 13:30:54 +0530

    user self following button removed

--- client/src/components/following Button/HandleFollowBtn.jsx
+++ client/src/components/following Button/HandleFollowBtn.jsx
@@ -8,8 +8,18 @@ const HandleFollowBtn = ({ profileUserId }) => {
 
   const { followingHandling } = useHandleFollowing();
 
+  const disableBtnStyle = {
+    // backgroundColor: currentUser?._id === profileUserId && "gray",
+    // PointerEvent: currentUser?._id === profileUserId && "none",
+    // cursor: currentUser?._id === profileUserId && "not-allowed",
+    display: currentUser?._id === profileUserId && "none",
+  };
+
   return (
-    <button onClick={() => followingHandling(profileUserId)}>
+    <button
+      onClick={() => followingHandling(profileUserId)}
+      style={disableBtnStyle}
+    >
       {profileUserId
         ? currentUser?.followings?.includes(profileUserId)
           ? "Unfollow"
