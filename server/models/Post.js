4321cdf9fec4d1288dfcbbb7032bcd74f76f8bab
Ashish Nagar <Ashishnagar033@gmail.com>
2023-11-08 21:30:09 +0530

    comment creation date added

--- server/models/Post.js
+++ server/models/Post.js
@@ -49,6 +49,10 @@ const postSchema = new mongoose.Schema(
           type: String,
           required: true,
         },
+        createdAt: {
+      type: Date,
+      default: Date.now,
+    },
       },
     ], 
   },
